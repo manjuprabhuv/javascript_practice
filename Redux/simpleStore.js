d18d10962b46d19050702ba6ea9f3870fa4ea0e7
Manju Prabhu <manjuprabhuv@gmail.com>
2019-02-04 23:07:17 +1100

    initial commit

---
+++ Redux/simpleStore.js
@@ -0,0 +1,97 @@
+const createStore = (reducer)=>{
+
+  let state;
+  let listeners =[];
+
+
+  const getState =()=>{
+
+    return state
+  }
+
+  const setState =(s)=>{
+    state = s;
+    listeners.forEach((listener)=>{
+      listener(state);
+    })
+  }
+
+  const subscribe = (listner)=>{
+
+    listeners.push(listner);
+
+    return(()=>{
+      listeners.filter((l)=>l!==listner)
+    })
+
+  }
+  const dispatch = (action)=>{
+    console.log("in dispatch",action);
+  	state= reducer(action); 
+   listeners.forEach((listener)=>{
+      listener(state);
+   })
+  }
+ 
+  return{
+    dispatch,
+    setState,
+    subscribe
+  }
+}
+
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+const reducer =()=>{
+  
+}
+
+const todoReducer = (state=[],action)=>{
+  console.log(action)
+  switch(action.type){
+    case ADD_TODO : 
+      return state.conact([action.todo]);
+    case REMOVE_TODO:
+      return state.filter((todo)=>action.id!==todo.id);
+    case TOGGLE_TODO:
+      return state.map((todo)=>{
+        todo.id!==action.id?todo :Object.assign({}, todo, { complete: !todo.complete })
+      });
+    default:
+      return state;
+  }
+  
+}
+
+const goalReducer = (state=[],action)=>{
+  
+  switch(action.type){
+    case ADD_GOAL : 
+      return state.conact([action.goal]);
+    case REMOVE_GOAL:
+      return state.filter((goal)=>action.id!==todo.id);
+    default:
+      return state;
+  }
+  
+}
+
+const rootReducer =(state={},action)=>{
+  console.log("root reducer ",action);
+ return{
+   todos: todoReducer(state.todos,action),
+   goals: goalReducer(state.goals,action)
+ }
+}
+
+
+
+let store = createStore(rootReducer);
+let unsubscribe = store.subscribe((state)=>console.log("The new state is =>",state));
+
+store.dispatch({type:ADD_TODO,todo:{id:1,name:"learn  React ",complete:false}});
+
